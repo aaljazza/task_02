b49f33d92dc9c6a1062629a268429d0a4b7432e9
Abdulla AlJazzaf <jazzaf@Abdullas-MacBook-Pro.local>
2018-07-02 19:52:27 +0300

    This is the updated version of Task 2; it worked

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def some_function(request):
+    context = {
+        "msg": "Hello World!",
+        "msg2": "this is my second test",
+        "msg3":"this worked",
+    }
+    return render(request, 'task_2_view.html', context)
